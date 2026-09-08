d3f3619575e8dc85bf61cfa60e31bf21351cff3e
Nihal Mallikarjun <nihal77778@gmail.com>
2026-09-08 13:03:07 -0500

    Add: first-unique-character-in-a-string - submission-0

---
+++ Data Structures & Algorithms/first-unique-character-in-a-string/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        hashmap = {}
+
+        for char in s:
+            if char in hashmap:
+                hashmap[char]+=1
+
+            else:
+                hashmap[char]=1
+
+        for i in range(len(s)):
+            if hashmap[s[i]]==1:
+                return i
+
+        return -1
