d113eac31f873f8a66fce75c2ab627fbdb6794f3
Nihal Mallikarjun <nihal77778@gmail.com>
2026-09-09 08:25:43 -0500

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,36 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+
+        hashmap1 = {}
+        hashmap2 = {}
+
+        for char in s:
+            if char in hashmap1:
+                hashmap1[char]+=1
+
+            else:
+                hashmap1[char]=1
+
+    
+        for char in t:
+            if char in hashmap2:
+                hashmap2[char]+=1
+
+            else:
+                hashmap2[char]=1
+
+        
+
+            if hashmap1==hashmap2:
+                return True
+
+        return False
+
+
+    
+
+        
+        
+        
